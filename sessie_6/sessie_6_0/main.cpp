6c5ab2da3ec3215c2660d73668e6fabbda9091bf
JosseVanDelm <josse.vandelm@gmail.com>
2018-12-10 21:11:45 +0100

    finish task 1;2 begin task 3

--- sessie_6/sessie_6_0/main.cpp
+++ sessie_6/sessie_6_0/main.cpp
@@ -12,9 +12,8 @@ https://docs.opencv.org/3.4/db/d28/tutorial_cascade_classifier.html
 */
 
 //prototypes
-void detectAndDisplay( Mat frame );
 
-CascadeClassifier face_cascade;
+Mat detectAndDisplay( Mat detection_frame,Mat draw_frame, Scalar color, CascadeClassifier* classifier );
 
 int main(int argc, const char** argv){
     // Adding a command line parser
@@ -22,7 +21,11 @@ int main(int argc, const char** argv){
         "{help h usage ? | | show this message }"
         "{video          | | (required) path to input video}"
         "{model          | | (required) path to xml file containing classifier model}"
+	"{model2         | | (optional) path to xml file containing second classifier model}"
     );
+    bool multiple_models = false;
+    CascadeClassifier model_cascade;
+    CascadeClassifier model2_cascade;
     // show help if help is asked
     if (parser.has("help")){
         parser.printMessage();
@@ -32,13 +35,25 @@ int main(int argc, const char** argv){
     // parse arguments and check if they are entered correctly
     string video_location(parser.get<string>("video"));
     string model_location(parser.get<string>("model"));
+    string model2_location(parser.get<string>("model2"));
     if (video_location.empty()){
         cerr << "There must be something wrong with your arguments. Please check again." << endl;
         parser.printMessage();
         return -1;
     }
-    if( !face_cascade.load( model_location) ){
-        cerr << "Error loading face cascade" << endl;
+    if (model2_location.empty()){
+        cerr << "No second model was given" << endl;
+    }
+    else{
+    	cerr << "using second model as well" << endl;
+	multiple_models = true;
+	if(!model2_cascade.load(model_location)){
+		cerr << "error loading second model" << endl;
+		return -1;
+	}
+    }
+    if( !model_cascade.load( model_location) ){
+        cerr << "Error loading first model" << endl;
         return -1;
     };
     VideoCapture cap(video_location);
@@ -49,6 +64,7 @@ int main(int argc, const char** argv){
     }
     while(1){
       Mat frame;
+      Mat frame_detection;
       // Capture frame-by-frame
       cap >> frame;
       // If the frame is empty, break immediately
@@ -56,33 +72,44 @@ int main(int argc, const char** argv){
         cerr << "Frame was empty. Quitting..." << endl;
         break;
       }
-      // Display the resulting frame
-      imshow( "Frame", frame );
-      detectAndDisplay(frame);
+      // Display the resulting frame (optional)
+      // imshow( "Frame", frame );
+      frame_detection = detectAndDisplay(frame,frame,Scalar(0,0,255),&model_cascade);
+      if(multiple_models) frame_detection = detectAndDisplay(frame,frame_detection,Scalar(0,255,0),&model2_cascade);
+      imshow( "Face detection", frame_detection );
       // Press  ESC on keyboard to exit
       char c=(char)waitKey(25);
       if(c==27)
         break;
     }
 }
-
-void detectAndDisplay( Mat frame )
+/*
+ * This function detects objects:
+ * Input:
+ * 	- classifier: pointer to used CascadeClassifier
+ * 	- color: scalar that represents color for drawing ellipses and text
+ * 	- detection_frame: frame that the objectdetector uses to detect objects with given classifier
+ * 	- draw_frame: frame that ellipses and text is drawn on.
+ * Output:
+ * 	- draw_frame with drawn detections and detection scores.
+ */
+Mat detectAndDisplay( Mat detection_frame,Mat draw_frame, Scalar color, CascadeClassifier* classifier )
 {
     Mat frame_gray;
-    cvtColor( frame, frame_gray, COLOR_BGR2GRAY );
+    cvtColor( detection_frame, frame_gray, COLOR_BGR2GRAY );
     equalizeHist( frame_gray, frame_gray );
     //-- Detect faces
     std::vector<Rect> faces;
     std::vector<int> detections;
-    face_cascade.detectMultiScale( frame_gray, faces, detections );
+    classifier->detectMultiScale( frame_gray, faces, detections );
     for ( size_t i = 0; i < faces.size(); i++ )
     {
         Point center( faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2 );
-        ellipse( frame, center, Size( faces[i].width/2, faces[i].height/2 ), 0, 0, 360, Scalar( 255, 0, 255 ), 4 );
-        putText(frame,to_string(detections[i]),Point(faces[i].x,faces[i].y),FONT_HERSHEY_COMPLEX,1,Scalar(0,255,0));
+        ellipse( draw_frame, center, Size( faces[i].width/2, faces[i].height/2 ), 0, 0, 360, color, 4 );
+        putText(draw_frame,to_string(detections[i]),Point(faces[i].x,faces[i].y),FONT_HERSHEY_COMPLEX,1,color);
     }
     //-- Show what you got
-    imshow( "Face detection", frame );
+    return draw_frame;
 }
 
 
